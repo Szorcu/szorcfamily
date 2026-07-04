c4e6de41e6b55c3e9097644569317256475d952d
Norbert Szorc <n.szorc@gmail.com>
2026-07-04 19:30:41 +0200

    Added shadcn

---
+++ src/components/atoms/Button/Button.types.ts
@@ -0,0 +1,8 @@
+import type { VariantProps } from "class-variance-authority";
+import type { ComponentProps } from "react";
+import type { buttonVariants } from "./Button";
+
+export type ButtonProps = ComponentProps<"button"> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean;
+  };
